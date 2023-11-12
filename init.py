3ada7f992316589816e74de23e1168b0aacf0ff8
awu <wualexander518@gmail.com>
2023-11-12 16:53:31 -0500

    updated homepage to show all available flights

--- init.py
+++ init.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 import pymysql.cursors
 
 app = Flask(__name__)
@@ -6,15 +6,19 @@ app = Flask(__name__)
 conn = pymysql.connect(host='localhost',
                        port = 3306,
                        user='root',
-                       password='root',
+                       password='',
                        db='ticket_reservation_system',
                        charset='utf8mb4',
                        cursorclass=pymysql.cursors.DictCursor) 
 
 @app.route('/')
-def hello():
-    return "Hello World"
+def root():
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM flight, airport WHERE flight.depart_from = airport.airport_code ORDER BY depart_date")
+    flights = cursor.fetchall()
+    return render_template("index.html", flights = flights)
 
+app.secret_key = 'some key that you will never guess'
 if __name__ == "__main__":
     app.run('127.0.0.1', 5000, debug=True)
 
