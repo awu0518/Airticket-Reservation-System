2dd2dc34ded7208dfdb24a61410e8d7ebd682a2d
awu <wualexander518@gmail.com>
2023-11-14 11:14:07 -0500

    initial search bar and some css changes

--- init.py
+++ init.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import pymysql.cursors
 
 app = Flask(__name__)
@@ -14,9 +14,21 @@ conn = pymysql.connect(host='localhost',
 @app.route('/')
 def root():
     cursor = conn.cursor()
+    cursor.execute("SELECT airport_name FROM airport")
+    airports = cursor.fetchall()
     cursor.execute("SELECT * FROM flight, airport WHERE flight.depart_from = airport.airport_code ORDER BY depart_date")
     flights = cursor.fetchall()
-    return render_template("index.html", flights = flights)
+    return render_template("index.html", airports = airports, flights = flights)
+
+@app.route('/searchFlights', methods=['GET', 'POST'])
+def searchFlight():
+    depart_from = request.form['departure_airport']
+    arrive_to = request.form['arrival_airport']
+    print(depart_from)
+    print(arrive_to)
+    
+    return render_template("index.html")
+
 
 app.secret_key = 'some key that you will never guess'
 if __name__ == "__main__":
