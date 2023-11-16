5ad8739f162395025b890b968e13117b4ef378e3
awu <wualexander518@gmail.com>
2023-11-16 18:42:48 -0500

    added status check

--- helper.py
+++ helper.py
@@ -3,3 +3,8 @@ def getAirports(conn):
     cursor.execute("SELECT airport_city FROM airport")
     return cursor.fetchall()
 
+def getAirlines(conn):
+    cursor = conn.cursor()
+    cursor.execute("SELECT airline_name FROM airline")
+    return cursor.fetchall()
+
--- init.py
+++ init.py
@@ -18,7 +18,8 @@ def root():
     query = "SELECT airline_name, T1.flight_num, T1.airport_city as depart_city, depart_date, depart_time, T2.airport_city as arrival_city, arrival_date, arrival_time, base_price, status FROM (SELECT airline_name, flight_num, airport_city, depart_date, depart_time, base_price, status FROM flight, airport WHERE depart_from = airport_code) as T1, (SELECT flight_num, airport_city, arrival_date, arrival_time FROM flight, airport where arrive_at = airport_code) as T2 WHERE T1.flight_num = T2.flight_num ORDER BY depart_date"
     cursor.execute(query)
     flights = cursor.fetchall()
-    return render_template("index.html", airports = getAirports(conn), flights = flights, search=True)
+    return render_template("index.html", airlines = getAirlines(conn), 
+                           airports = getAirports(conn), flights = flights, search=True)
 
 @app.route('/searchFlights', methods=['GET', 'POST'])
 def searchFlight():
@@ -37,10 +38,26 @@ def searchFlight():
         query = "SELECT airline_name, T1.flight_num, T1.airport_city as depart_city, depart_date, depart_time, T2.airport_city as arrival_city, arrival_date, arrival_time, base_price, status FROM (SELECT airline_name, flight_num, airport_city, depart_date, depart_time, base_price, status FROM flight, airport WHERE depart_from = airport_code) as T1, (SELECT flight_num, airport_city, arrival_date, arrival_time FROM flight, airport where arrive_at = airport_code) as T2 WHERE T1.flight_num = T2.flight_num and T1.airport_city = %s and T2.airport_city = %s and depart_date = %s"
         cursor.execute(query, (arrive_to, depart_from, return_date))
         return_flights = cursor.fetchall()
-        return render_template("index.html", airports=getAirports(conn), flights=depart_flights, rflights = return_flights, search=False)
+        return render_template("index.html", airlines = getAirlines(conn), airports=getAirports(conn), 
+                               flights=depart_flights, rflights = return_flights, search=False)
     else:
-        return render_template("index.html", airports=getAirports(conn), flights=depart_flights, search=False) 
+        return render_template("index.html", airlines = getAirlines(conn), 
+                               airports=getAirports(conn), flights=depart_flights, search=False) 
 
+@app.route('/checkStatus', methods=['GET', 'POST'])
+def checkStatus():
+    cursor = conn.cursor()
+
+    airline_name = request.form['airline_name']
+    flight_id = str(request.form['flight_id'])
+    depart_date = str(request.form['departure_date'])
+
+    query = "SELECT airline_name, T1.flight_num, T1.airport_city as depart_city, depart_date, depart_time, T2.airport_city as arrival_city, arrival_date, arrival_time, base_price, status FROM (SELECT airline_name, flight_num, airport_city, depart_date, depart_time, base_price, status FROM flight, airport WHERE depart_from = airport_code) as T1, (SELECT flight_num, airport_city, arrival_date, arrival_time FROM flight, airport where arrive_at = airport_code) as T2 WHERE T1.flight_num = T2.flight_num and T1.airline_name = %s and T1.flight_num = %s and depart_date = %s"
+    cursor.execute(query, (airline_name, flight_id, depart_date))
+    result = cursor.fetchone()
+
+    return render_template("index.html", result = result, search=False)
+    
 @app.route('/login')
 def login():
     return render_template("login/login.html")
