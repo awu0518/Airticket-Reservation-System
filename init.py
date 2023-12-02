3066c6f5057f4f24ff6bc62fc4ce1ccdf4737fdb
awu <wualexander518@gmail.com>
2023-12-02 17:32:09 -0500

    reorganized init.py

--- init.py
+++ init.py
@@ -15,6 +15,7 @@ conn = pymysql.connect(host='localhost',
                        charset='utf8mb4',
                        cursorclass=pymysql.cursors.DictCursor) 
 
+# DEFAULT PAGE AND FUNCTIONALITY 
 @app.route('/')
 def root():
     cursor = conn.cursor()
@@ -69,6 +70,7 @@ def login():
 def register():
     return render_template("login/register.html")
 
+# STAFF PAGE AND FUNCTIONALITY 
 @app.route('/staffLoginAuth', methods=['GET', 'POST'])
 def loginStaff():
     username = request.form['username']
@@ -88,76 +90,10 @@ def loginStaff():
         return redirect(url_for("staffHome"))
     else:
         return render_template("login/login.html", error = "Incorrect credentials")
-    
-@app.route('/custLoginAuth', methods=['GET', 'POST'])
-def loginCustomer():
-    username = request.form['username']
-    password = request.form['password']
-
-    password = hashlib.md5(password.encode())
-    password = password.hexdigest()
-
-    cursor = conn.cursor()
-    query = "SELECT * FROM customer WHERE username = %s and password = %s"
-    cursor.execute(query, (username, password))
-
-    data = cursor.fetchone()
-    
-    if (data):
-        session['username'] = username
-        return redirect(url_for("custHome"))
-    else:
-        return render_template("login/login.html", error = "Incorrect credentials")
-
-@app.route('/getCustReg')
-def getCustReg():
-    return render_template("/login/customerReg.html")
 
 @app.route('/getStaffReg')
 def getStaffReg():
     return render_template("/login/staffReg.html", airlines=getAirlines(conn))
-
-@app.route('/customerRegistration', methods=['GET', 'POST'])
-def customerRegistration():
-    cursor = conn.cursor()
-    email = request.form['email']
-
-    query = "SELECT * FROM customer WHERE email = %s"
-    cursor.execute(query, email)
-    data = cursor.fetchone()
-
-    if data is not None:
-        return render_template("/login/customerReg.html", error="Account already associated with email, please login")
-    
-    password = request.form['password']
-    password = hashlib.md5(password.encode())
-    password = password.hexdigest()
-
-    f_name = request.form['first_name']
-    l_name = request.form['last_name']
-    phone = str(request.form['phone_number'])
-    dob = str(request.form['dob'])
-
-    b_num = request.form['building_num']
-    street = request.form['street']
-    a_num = request.form['apartment_num']
-    city = request.form['city']
-    state = request.form['state']
-    zip = request.form['zip_code']
-
-    p_num = request.form['passport_num']
-    p_exp = str(request.form['passport_exp'])
-    p_country = request.form['passport_country']
-    
-    query = "insert into Customer values (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
-    cursor.execute(query, (email, password, f_name, l_name, b_num, street, a_num,
-          city, state, zip, p_num, p_exp, p_country, dob))
-    query = "insert into Phone_Number values (%s, %s)"
-    cursor.execute(query, (email, phone))
-
-    conn.commit()
-
-    return redirect(url_for("login"))
 
 @app.route('/staffRegistration', methods=['GET', 'POST'])
 def staffReg():
@@ -192,16 +128,6 @@ def staffReg():
     conn.commit()
 
     return redirect(url_for("login"))
-
-@app.route('/custHome')
-def custHome():
-    username = session['username']
-
-    cursor = conn.cursor()
-    query = 'SELECT * FROM Customer WHERE email=%s'
-    cursor.execute(query, (username))
-
-    return render_template('/customer/home.html', username=username, data=cursor.fetchone())
 
 @app.route('/staffHome')
 def staffHome():
@@ -426,6 +352,83 @@ def getFlightsFromCustomer():
     flights = cursor.fetchall()
     
     return render_template('/staff/customerFlights.html', flights=flights, cust_email=cust_email)
+
+# CUSTOMER PAGE AND FUNCTIONALITY
+@app.route('/custLoginAuth', methods=['GET', 'POST'])
+def loginCustomer():
+    username = request.form['username']
+    password = request.form['password']
+
+    password = hashlib.md5(password.encode())
+    password = password.hexdigest()
+
+    cursor = conn.cursor()
+    query = "SELECT * FROM customer WHERE email = %s and password = %s"
+    cursor.execute(query, (username, password))
+
+    data = cursor.fetchone()
+    
+    if (data):
+        session['username'] = username
+        return redirect(url_for("custHome"))
+    else:
+        return render_template("login/login.html", error = "Incorrect credentials")
+
+@app.route('/getCustReg')
+def getCustReg():
+    return render_template("/login/customerReg.html")
+
+@app.route('/customerRegistration', methods=['GET', 'POST'])
+def customerRegistration():
+    cursor = conn.cursor()
+    email = request.form['email']
+
+    query = "SELECT * FROM customer WHERE email = %s"
+    cursor.execute(query, email)
+    data = cursor.fetchone()
+
+    if data is not None:
+        return render_template("/login/customerReg.html", error="Account already associated with email, please login")
+    
+    password = request.form['password']
+    password = hashlib.md5(password.encode())
+    password = password.hexdigest()
+
+    f_name = request.form['first_name']
+    l_name = request.form['last_name']
+    phone = str(request.form['phone_number'])
+    dob = str(request.form['dob'])
+
+    b_num = request.form['building_num']
+    street = request.form['street']
+    a_num = request.form['apartment_num']
+    city = request.form['city']
+    state = request.form['state']
+    zip = request.form['zip_code']
+
+    p_num = request.form['passport_num']
+    p_exp = str(request.form['passport_exp'])
+    p_country = request.form['passport_country']
+    
+    query = "insert into Customer values (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
+    cursor.execute(query, (email, password, f_name, l_name, b_num, street, a_num,
+          city, state, zip, p_num, p_exp, p_country, dob))
+    query = "insert into Phone_Number values (%s, %s)"
+    cursor.execute(query, (email, phone))
+
+    conn.commit()
+
+    return redirect(url_for("login"))
+
+@app.route('/custHome')
+def custHome():
+    username = session['username']
+
+    cursor = conn.cursor()
+    query = 'SELECT * FROM Customer WHERE email=%s'
+    cursor.execute(query, (username))
+
+    return render_template('/customer/home.html', username=username, data=cursor.fetchone())
 
 @app.route('/logout')
 def logout():
